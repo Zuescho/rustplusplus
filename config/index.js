83295a605785fe320f5c170945c28be8a2127aa8
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-09-07 18:21:14 -0500

    .

--- config/index.js
+++ config/index.js
@@ -26,9 +26,9 @@ module.exports = {
         reconnectIntervalMs: process.env.RPP_RECONNECT_INTERVAL || 15000,
     },
     discord: {
-        username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
-        clientId: process.env.RPP_DISCORD_CLIENT_ID || '',
-        token: process.env.RPP_DISCORD_TOKEN || '',
+        username: process.env.RPP_DISCORD_USERNAME || '',
+        clientId: process.env.RPP_DISCORD_CLIENT_ID || '1220250556652720148',
+        token: process.env.RPP_DISCORD_TOKEN || 'MTIyMDI1MDU1NjY1MjcyMDE0OA.G4fsCJ.07aWFBcjWYTB5qWdsuTupZ4eXQgasWX2CIBXqQ',
         needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true, /* If true, only admins can delete (server, switch..), manage credentials and reset a channel */
     }
 };
